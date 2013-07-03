bc47242b87da22569438caa2c0fd7e769f0c1e7b
Michel1975 <michelhansen@disruptx.dk>
2013-07-03 08:59:57 +0200

    rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Delta1::Application.config.secret_token = '86f74bbbc67e9aaa8d14fca678bdfd6bdc7c49b194489075c796f3a2435f5a5b8f992e23d3801648f5aa5895059094637e9cf0b357c2fa008f2cd350e6dc6c74'
